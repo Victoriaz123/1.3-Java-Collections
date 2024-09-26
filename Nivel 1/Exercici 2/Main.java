ef8eeabd2c782757690ea1acdddcdc54cd61cb35
Victoriaz123 <148845558+Victoriaz123@users.noreply.github.com>
2024-09-26 11:19:56 +0200

    Update Main.java

--- Nivel 1/Exercici 2/Main.java
+++ Nivel 1/Exercici 2/Main.java
@@ -5,7 +5,6 @@ import java.util.ListIterator;
 public class Main {
     public static void main(String[] args) {
 
-        // create new list and insert some elements
     List<Integer> list =new ArrayList<Integer>();
     list.add(1);
     list.add(2);
@@ -14,18 +13,14 @@ public class Main {
     list.add(5);
     System.out.println("First list"+ list);
 
-         // create 2nd list
     List<Integer> list2= new ArrayList<Integer>();
 
-        //Generate an iterator to iterate List in reverse order
-        ListIterator<Integer>listIterator=list.listIterator(list.size());
+    ListIterator<Integer>listIterator=list.listIterator(list.size());
 
-        //hasPrevious() returns true if the list has previous element
-     while (listIterator.hasPrevious()) {
-            list2.add(listIterator.previous());
+    while (listIterator.hasPrevious()) {
+          list2.add(listIterator.previous());
         }
 
-        // print the second list in reverse order
         System.out.println("Second list in reverse order: " + list2);
         }
 }
